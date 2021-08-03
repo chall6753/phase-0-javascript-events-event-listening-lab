e8b2a77e5917b2c14434f3473927d394186f8ac7
Charles <chall70101@gmail.com>
2021-08-02 21:17:25 -0600

    commit tree 8335c479fa41d709f676d3e641a4eb27a4c474dc

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 function addingEventListener() {
-    const input = document.getElementById('input');
-
-function clickAlert() {
-  alert('I was clicked!');
-}
-
-input.addEventListener('click', clickAlert); 
+      document.getElementById('input').addEventListener('click', () => alert('I was clicked'))
 }
 
 addingEventListener()
